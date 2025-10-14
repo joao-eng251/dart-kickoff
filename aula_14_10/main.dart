da9af6fbc3512e0e8cb3564c98126da84d0a9d76
Fabio Barros <you@example.com>
2025-10-14 20:08:42 -0300

    commit inicial

---
+++ aula_14_10/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  String meuNome = 'Joao Vitor';
+  int idade = 14;
+  double altura = 1.69;
+  bool resultado = 10 < 5;
+  print(meuNome);
+  print(idade);
+  print(altura);
+  print(resultado);
+}
